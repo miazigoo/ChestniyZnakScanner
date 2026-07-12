19af8832f2f16c7457744eac752f79ded79f9a63
miazigoo <kasper6024@gmail.com>
2026-07-12 18:34:26 +0300

    chore: bump TSD app version to 1.0.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "ru.devandprod.chestniyznak"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.0.14"
+        versionCode = 16
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
